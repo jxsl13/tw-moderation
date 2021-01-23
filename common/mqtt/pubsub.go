a94cdbf8eb90480ed94540656f67d3e2bcf0dfe7
jxsl13 <jxsl13@gmail.com>
2021-01-23 03:05:36 +0100

    testing different pubsub

--- common/mqtt/pubsub.go
+++ common/mqtt/pubsub.go
@@ -1,80 +1,85 @@
 package mqtt
 
-import (
-	"log"
-	"time"
-
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-)
-
 // NewPublisherSubscriber creates a Publisher as well as a Subscriber from a single connection.
 func NewPublisherSubscriber(address, clientID, publishTopic, subscribeTopic string) (*Publisher, *Subscriber, error) {
-	publisher := &Publisher{
-		address:    address,
-		clientID:   clientID,
-		topic:      publishTopic,
-		client:     nil,
-		msgChannel: make(chan string, 1024),
+	pub, err := NewPublisher(address, clientID+"pub", publishTopic)
+	if err != nil {
+		return nil, nil, err
 	}
 
-	subscriber := &Subscriber{
-		address:    address,
-		clientID:   clientID,
-		topic:      subscribeTopic,
-		client:     nil,
-		msgChannel: make(chan string, 1024),
+	sub, err := NewSubscriber(address, clientID+"sub", subscribeTopic)
+	if err != nil {
+		return nil, nil, err
 	}
 
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(address)
-	opts.SetClientID(clientID)
-	opts.SetKeepAlive(10 * time.Second)
-	opts.SetPingTimeout(1 * time.Second)
-	opts.SetAutoReconnect(true)
-	opts.OnConnect = func(_ mqtt.Client) {
-		log.Println("PubSub connected to", address, "with ID:", clientID)
-	}
-	opts.OnConnectionLost = func(_ mqtt.Client, err error) {
-		log.Println("PubSub lost connection of", address, "with ID:", clientID, "error:", err)
-	}
-	opts.OnReconnecting = func(client mqtt.Client, options *mqtt.ClientOptions) {
-		log.Println("PubSub reconnected to", address, "with ID:", clientID)
-	}
+	return pub, sub, nil
 
-	c := mqtt.NewClient(opts)
-	publisher.client = c
-	subscriber.client = c
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		publisher.Close()
-		subscriber.Close()
-		return nil, nil, token.Error()
-	}
+	// publisher := &Publisher{
+	// 	address:    address,
+	// 	clientID:   clientID,
+	// 	topic:      publishTopic,
+	// 	client:     nil,
+	// 	msgChannel: make(chan string, 1024),
+	// }
 
-	// create publishing routine
-	go func() {
-		for msg := range publisher.msgChannel {
-			if t := publisher.client.Publish(publishTopic, 1, false, msg); t.Wait() && t.Error() != nil {
-				log.Println("Publisher could not send message to", publisher.address, "on topic", publisher.topic)
-			} else {
-				log.Println("Published message:", msg)
-			}
-		}
-	}()
+	// subscriber := &Subscriber{
+	// 	address:    address,
+	// 	clientID:   clientID,
+	// 	topic:      subscribeTopic,
+	// 	client:     nil,
+	// 	msgChannel: make(chan string, 1024),
+	// }
 
-	// Subscribe to second topic
-	subscriber.client = c
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		publisher.Close()
-		subscriber.Close()
-		return nil, nil, token.Error()
-	}
+	// opts := mqtt.NewClientOptions()
+	// opts.AddBroker(address)
+	// opts.SetClientID(clientID)
+	// opts.SetKeepAlive(10 * time.Second)
+	// opts.SetPingTimeout(1 * time.Second)
+	// opts.SetAutoReconnect(true)
+	// opts.OnConnect = func(_ mqtt.Client) {
+	// 	log.Println("PubSub connected to", address, "with ID:", clientID)
+	// }
+	// opts.OnConnectionLost = func(_ mqtt.Client, err error) {
+	// 	log.Println("PubSub lost connection of", address, "with ID:", clientID, "error:", err)
+	// }
+	// opts.OnReconnecting = func(client mqtt.Client, options *mqtt.ClientOptions) {
+	// 	log.Println("PubSub reconnected to", address, "with ID:", clientID)
+	// }
 
-	fwdHandler := subscriber.getForwardHandler()
-	if token := c.Subscribe(subscribeTopic, 1, fwdHandler); token.Wait() && token.Error() != nil {
-		publisher.Close()
-		subscriber.Close()
-		return nil, nil, token.Error()
-	}
+	// c := mqtt.NewClient(opts)
+	// publisher.client = c
+	// subscriber.client = c
+	// if token := c.Connect(); token.Wait() && token.Error() != nil {
+	// 	publisher.Close()
+	// 	subscriber.Close()
+	// 	return nil, nil, token.Error()
+	// }
 
-	return publisher, subscriber, nil
+	// // create publishing routine
+	// go func() {
+	// 	for msg := range publisher.msgChannel {
+	// 		if t := publisher.client.Publish(publishTopic, 1, false, msg); t.Wait() && t.Error() != nil {
+	// 			log.Println("Publisher could not send message to", publisher.address, "on topic", publisher.topic)
+	// 		} else {
+	// 			log.Println("Published message:", msg)
+	// 		}
+	// 	}
+	// }()
+
+	// // Subscribe to second topic
+	// subscriber.client = c
+	// if token := c.Connect(); token.Wait() && token.Error() != nil {
+	// 	publisher.Close()
+	// 	subscriber.Close()
+	// 	return nil, nil, token.Error()
+	// }
+
+	// fwdHandler := subscriber.getForwardHandler()
+	// if token := c.Subscribe(subscribeTopic, 1, fwdHandler); token.Wait() && token.Error() != nil {
+	// 	publisher.Close()
+	// 	subscriber.Close()
+	// 	return nil, nil, token.Error()
+	// }
+
+	// return publisher, subscriber, nil
 }
--- publisher/go.mod
+++ publisher/go.mod
@@ -3,6 +3,6 @@ module github.com/jxsl13/tw-moderation/publisher
 go 1.15
 
 require (
-	github.com/jxsl13/tw-moderation v0.0.0-20210122234039-27f8b4f4e712
+	github.com/jxsl13/tw-moderation v0.0.0-20210123015858-a04b96fc70d1
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
 )
