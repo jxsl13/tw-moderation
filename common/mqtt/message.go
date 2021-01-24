0f3e2eeda786196a1add97ad94ccd92b77087aa4
jxsl13 <jxsl13@gmail.com>
2021-01-24 15:26:11 +0100

    fix typo

--- common/mqtt/message.go
+++ common/mqtt/message.go
@@ -3,6 +3,6 @@ package mqtt
 // Message is a simple struct that allows to publish to different topics
 // with a single publisher client connection
 type Message struct {
-	Topic    string
-	Playload interface{}
+	Topic   string
+	Payload interface{}
 }
